bc36c97d3526236eb6d60aa283f11712bbfdbdc0
Kirill Mil <kirill.mil.0310@mail.ru>
2022-07-14 22:38:18 +0700

    #2 - rewrite action and change queryset

--- server/apps/pockets/viewsets/transaction_category.py
+++ server/apps/pockets/viewsets/transaction_category.py
@@ -1,6 +1,7 @@
 from typing import Type
 
 from django.db.models import QuerySet
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework import viewsets, serializers
 from rest_framework.decorators import action
@@ -8,36 +9,28 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from ..filters import TransactionCategoryFilter
 from ..models import TransactionCategory
-from ..serializers import (
-    TransactionCategorySerializer,
-    TransactionCategoryTransactionSumSerializer,
-)
+from ..serializers import TransactionCategorySerializer, TransactionCategoryTransactionSumSerializer
 
 
 class TransactionCategoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TransactionCategoryFilter
 
     def get_serializer_class(self) -> Type[serializers.ModelSerializer]:
-        if self.action == 'transactions_by_categories':
-            serializer_class = TransactionCategoryTransactionSumSerializer
-        else:
-            serializer_class = TransactionCategorySerializer
-
-        return serializer_class
+        if self.action == 'expenses':
+            return TransactionCategoryTransactionSumSerializer
+        return TransactionCategorySerializer
 
     def get_queryset(self) -> QuerySet:
-        queryset = TransactionCategory.objects.filter(
+        return TransactionCategory.objects.filter(
             user=self.request.user,
-        ).order_by(
-            '-id',
+        ).annotate_with_transaction_sums().order_by(
+            '-transactions_sum',
         )
 
-        if self.action == 'transactions_by_categories':
-            queryset = queryset.annotate_with_transaction_sums()
-
-        return queryset
-
-    @action(methods=('GET',), detail=False, url_path='transactions-by-categories')
-    def transactions_by_categories(self, request: Request, *args, **kwargs) -> Response:
+    @action(methods=('GET',), detail=False)
+    def expenses(self, request: Request, *args, **kwargs) -> Response:
         return super().list(request, *args, **kwargs)
