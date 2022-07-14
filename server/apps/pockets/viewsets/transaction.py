f6918c7392424a31124738a6c6880a0f1e14bbb7
Kirill Mil <kirill.mil.0310@mail.ru>
2022-07-14 22:37:39 +0700

    #2 - add filters

--- server/apps/pockets/viewsets/transaction.py
+++ server/apps/pockets/viewsets/transaction.py
@@ -2,12 +2,14 @@ from decimal import Decimal
 from typing import Type, Union
 
 from django.utils import timezone
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, serializers, pagination
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from ..filters import TransactionFilter
 from ..models import Transaction
 from ..models.querysets import TransactionQuerySet
 from ..serializers import (
@@ -22,6 +24,9 @@ class TransactionViewSet(viewsets.ModelViewSet):
     pagination_class.default_limit = 20
     permission_classes = (IsAuthenticated,)
 
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TransactionFilter
+
     def get_serializer_class(self) -> Type[serializers.ModelSerializer]:
         if self.action == 'total':
             serializer_class = TransactionGlobalSerializer
@@ -33,13 +38,15 @@ class TransactionViewSet(viewsets.ModelViewSet):
         return serializer_class
 
     def get_queryset(self) -> TransactionQuerySet:
-        return Transaction.objects.filter(
+        queryset = Transaction.objects.filter(
             user=self.request.user,
         ).select_related(
             'category',
         ).order_by(
             '-transaction_date', '-id',
         )
+
+        return queryset
 
     def get_object(self) -> Union[Transaction, dict[str, Decimal]]:
         if self.action == 'total':
